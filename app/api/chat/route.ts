8fa649150277ad66b361a7e4c9107daf460efe1c
jung <jungno@gmail.com>
2025-07-26 23:28:12 -0600

    fontendtool Used tool: weather

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,26 +1,36 @@
 import { openai } from "@ai-sdk/openai";
+import { google } from "@ai-sdk/google";
 import { ollama } from "ollama-ai-provider"
 import { frontendTools } from "@assistant-ui/react-ai-sdk";
 import { streamText } from "ai";
+import {z} from "zod";
+
 import { experimental_createMCPClient as createMCPClient } from "ai";
 
 export const runtime = "edge";
 export const maxDuration = 30;
 
-
-
 export async function POST(req: Request) {
   const { messages, system, tools } = await req.json();
 
   const result = streamText({
-    //model: openai("gpt-4o"),
-    model: ollama("gemma3:4b-it-qat"),
+    model: google("gemini-2.0-flash"),
+    //model: ollama("qwen3:4b"),
     messages,
     // forward system prompt and tools from the frontend
     toolCallStreaming: true,
     system,
     tools: {
       ...frontendTools(tools),
+      weather: {
+        description: "Get weather information",
+        parameters: z.object({
+          location: z.string().describe("Location to get weather for"),
+        }),
+        execute: async ({ location }) => {
+          return `The weather in ${location} is sunny.`;
+        },
+      },
     },
     onError: console.log,
   });
